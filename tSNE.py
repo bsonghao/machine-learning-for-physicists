6277630d4ef6f6c27a72fcb9f0eaa2d146017af0
bsonghao <bsonghao@gmail.com>
2023-04-25 15:33:45 -0400

    test on the tSNE approach on the MNIST data base

---
+++ tSNE.py
@@ -0,0 +1,254 @@
+
+# coding: utf-8
+
+# # Visualizing the autoencoder bottleneck via t-SNE
+
+# Example code for the lecture series "Machine Learning for Physicists" by Florian Marquardt
+#
+# Lecture 5, Homework (discussed in session 6)
+#
+# See https://machine-learning-for-physicists.org and the current course website linked there!
+
+# This notebook shows how to:
+# - visualize the bottleneck of convolutional autoencoders using t-SNE
+#
+# This is built on the basis of the 04_tutorial_CNNtraining notebook on autoencoders.
+#
+# The networks are 2D convolutional networks, with the same input and output dimensions, and a bottleneck layer in the middle.
+#
+# You define the network and the type of images that are generated for training – this notebook will help you visualize the training evolution.
+
+# ### Imports: numpy and matplotlib and keras
+
+# In[1]:
+
+
+# keras: Sequential is the neural-network class, Dense is
+# the standard network layer
+from tensorflow.keras import Sequential
+from tensorflow.keras import Model
+from tensorflow.keras.layers import Dense, Conv2D, AveragePooling2D, UpSampling2D
+from tensorflow.keras import optimizers # to choose more advanced optimizers like 'adam'
+
+import numpy as np
+
+import matplotlib.pyplot as plt # for plotting
+import matplotlib
+matplotlib.rcParams['figure.dpi']=300 # highres display
+
+# for subplots within subplots:
+from matplotlib import gridspec
+
+# for nice inset colorbars: (approach changed from lecture 1 'Visualization' notebook)
+from mpl_toolkits.axes_grid1.inset_locator import InsetPosition
+
+# for updating display
+# (very simple animation)
+from IPython.display import clear_output
+from time import sleep
+
+
+# The following python code for t-SNE is an original code by the inventor of t-SNE, Laurens van der Maaten. It is available on his website https://lvdmaaten.github.io/tsne/ .
+#
+# It is stated on that website: "*You are free to use, modify, or redistribute this software in any way you want, but only for non-commercial purposes. The use of the software is at your own risk; the authors are not responsible for any damage as a result from errors in the software.*"
+
+# In[41]:
+
+
+#
+#  tsne.py
+#
+# Implementation of t-SNE in Python. The implementation was tested on Python
+# 2.7.10, and it requires a working installation of NumPy. The implementation
+# comes with an example on the MNIST dataset. In order to plot the
+# results of this example, a working installation of matplotlib is required.
+#
+# The example can be run by executing: `ipython tsne.py`
+#
+#
+#  Created by Laurens van der Maaten on 20-12-08.
+#  Copyright (c) 2008 Tilburg University. All rights reserved.
+
+# note by FM: For this notebook, the MNIST example was dropped
+
+import numpy as np
+
+def Hbeta(D=np.array([]), beta=1.0):
+    """
+        Compute the perplexity and the P-row for a specific value of the
+        precision of a Gaussian distribution.
+    """
+
+    # Compute P-row and corresponding perplexity
+    P = np.exp(-D.copy() * beta)
+    sumP = sum(P)
+    H = np.log(sumP) + beta * np.sum(D * P) / sumP
+    P = P / sumP
+    return H, P
+
+
+def x2p(X=np.array([]), tol=1e-5, perplexity=30.0):
+    """
+        Performs a binary search to get P-values in such a way that each
+        conditional Gaussian has the same perplexity.
+    """
+
+    # Initialize some variables
+    print("Computing pairwise distances...")
+    (n, d) = X.shape
+    sum_X = np.sum(np.square(X), 1)
+    D = np.add(np.add(-2 * np.dot(X, X.T), sum_X).T, sum_X)
+    P = np.zeros((n, n))
+    beta = np.ones((n, 1))
+    logU = np.log(perplexity)
+
+    # Loop over all datapoints
+    for i in range(n):
+
+        # Print progress
+        if i % 10 == 0:
+            print("Computing P-values for point %d of %d..." % (i, n))
+
+        # Compute the Gaussian kernel and entropy for the current precision
+        betamin = -np.inf
+        betamax = np.inf
+        Di = D[i, np.concatenate((np.r_[0:i], np.r_[i+1:n]))]
+        (H, thisP) = Hbeta(Di, beta[i])
+
+        # Evaluate whether the perplexity is within tolerance
+        Hdiff = H - logU
+        tries = 0
+        while np.abs(Hdiff) > tol and tries < 50:
+
+            # If not, increase or decrease precision
+            if Hdiff > 0:
+                betamin = beta[i].copy()
+                if betamax == np.inf or betamax == -np.inf:
+                    beta[i] = beta[i] * 2.
+                else:
+                    beta[i] = (beta[i] + betamax) / 2.
+            else:
+                betamax = beta[i].copy()
+                if betamin == np.inf or betamin == -np.inf:
+                    beta[i] = beta[i] / 2.
+                else:
+                    beta[i] = (beta[i] + betamin) / 2.
+
+            # Recompute the values
+            (H, thisP) = Hbeta(Di, beta[i])
+            Hdiff = H - logU
+            tries += 1
+
+        # Set the final row of P
+        P[i, np.concatenate((np.r_[0:i], np.r_[i+1:n]))] = thisP
+
+    # Return final P-matrix
+    print("Mean value of sigma: %f" % np.mean(np.sqrt(1 / beta)))
+    return P
+
+
+def pca(X=np.array([]), no_dims=50):
+    """
+        Runs PCA on the NxD array X in order to reduce its dimensionality to
+        no_dims dimensions.
+    """
+
+    print("Preprocessing the data using PCA...")
+    (n, d) = X.shape
+    X = X - np.tile(np.mean(X, 0), (n, 1))
+    (l, M) = np.linalg.eig(np.dot(X.T, X))
+    Y = np.dot(X, M[:, 0:no_dims])
+    return Y
+
+
+def tsne(X=np.array([]), no_dims=2, initial_dims=50, perplexity=30.0,
+         do_animation=False, animation_skip_steps=10, max_iter = 1000):
+    """
+        Runs t-SNE on the dataset in the NxD array X to reduce its
+        dimensionality to no_dims dimensions. The syntaxis of the function is
+        `Y = tsne.tsne(X, no_dims, perplexity), where X is an NxD NumPy array.
+
+        Added by F. Marquardt: do_animation==True will give you a graphical animation of
+        the progress, use animation_skip_steps to control how often this will
+        be plotted; max_iter controls the total number of gradient descent steps
+    """
+
+    # Check inputs
+    if isinstance(no_dims, float):
+        print("Error: array X should have type float.")
+        return -1
+    if round(no_dims) != no_dims:
+        print("Error: number of dimensions should be an integer.")
+        return -1
+
+    # Initialize variables
+    X = pca(X, initial_dims).real
+    (n, d) = X.shape
+    initial_momentum = 0.5
+    final_momentum = 0.8
+    eta = 500
+    min_gain = 0.01
+    Y = np.random.randn(n, no_dims)
+    dY = np.zeros((n, no_dims))
+    iY = np.zeros((n, no_dims))
+    gains = np.ones((n, no_dims))
+
+    # Compute P-values
+    P = x2p(X, 1e-5, perplexity)
+    P = P + np.transpose(P)
+    P = P / np.sum(P)
+    P = P * 4.									# early exaggeration
+    P = np.maximum(P, 1e-12)
+
+    if do_animation: # added by FM
+        costs=np.zeros(max_iter) # to store the cost values
+
+    # Run iterations
+    for iter in range(max_iter):
+
+        # Compute pairwise affinities
+        sum_Y = np.sum(np.square(Y), 1)
+        num = -2. * np.dot(Y, Y.T)
+        num = 1. / (1. + np.add(np.add(num, sum_Y).T, sum_Y))
+        num[range(n), range(n)] = 0.
+        Q = num / np.sum(num)
+        Q = np.maximum(Q, 1e-12)
+
+        # Compute gradient
+        PQ = P - Q
+        for i in range(n):
+            dY[i, :] = np.sum(np.tile(PQ[:, i] * num[:, i], (no_dims, 1)).T * (Y[i, :] - Y), 0)
+
+        # Perform the update
+        if iter < 20:
+            momentum = initial_momentum
+        else:
+            momentum = final_momentum
+        gains = (gains + 0.2) * ((dY > 0.) != (iY > 0.)) +                 (gains * 0.8) * ((dY > 0.) == (iY > 0.))
+        gains[gains < min_gain] = min_gain
+        iY = momentum * iY - eta * (gains * dY)
+        Y = Y + iY
+        Y = Y - np.tile(np.mean(Y, 0), (n, 1))
+
+        if not do_animation: # added by FM: do not print if we are animating!
+            # Compute current value of cost function
+            if (iter + 1) % 10 == 0:
+                C = np.sum(P * np.log(P / Q))
+                print("Iteration %d: error is %f" % (iter + 1, C), end="           \r") # modified to overwrite line
+
+        # Stop lying about P-values
+        if iter == 100:
+            P = P / 4.
+
+        if do_animation:  # added by FM
+            C = np.sum(P * np.log(P / Q)) # compute for every step, to store it in 'costs'
+            costs[iter]=C
+            if iter % animation_skip_steps==0:
+                clear_output(wait=True)
+                fig,ax=plt.subplots(ncols=2,nrows=1,figsize=(10,5))
+                ax[0].plot(costs)
+                ax[1].scatter(Y[:,0],Y[:,1],color="orange")
+                plt.show()
+
+    # Return solution
+    return Y
